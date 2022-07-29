6ec0cc2e59f0dc44a85ba806eaa440cff76235ef
unknown <maccionidias@gmai.com>
2022-07-29 10:13:54 -0300

    CRUD Seasons

--- api-movies/src/routes/index.js
+++ api-movies/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 
 const MovieController = require("../controllers/MovieController");
+const SeasonController = require("../controllers/SeasonController");
 
 const routes = Router();
 
@@ -9,5 +10,12 @@ routes.get("/movies/:id", MovieController.getOne);
 routes.post("/movies", MovieController.store);
 routes.put("/movies/:id", MovieController.update);
 routes.delete("/movies/:id", MovieController.delete);
+
+
+routes.get("/movies/:id_movie/seasons", SeasonController.getAll);
+routes.get("/movies/season/:id", SeasonController.getOne);
+routes.post("/movies/:id_movie/season", SeasonController.store);
+routes.put("/movies/:id_movie/season/:id", SeasonController.update);
+routes.delete("/season/:id", SeasonController.delete);
 
 module.exports = { routes };
